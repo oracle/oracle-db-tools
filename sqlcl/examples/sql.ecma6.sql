ff0ee53267b801108393c7cf591160b68d4ba32b
Kris Rice <kris.rice@jokr.net>
2016-11-17 09:44:32 -0500

    Create sql.ecma6.sql

---
+++ sqlcl/examples/sql.ecma6.sql
@@ -0,0 +1,38 @@
+/* Look up a single value to use in a bind later */
+var user = util.executeReturnOneCol('select user from dual');
+
+var binds = {};
+binds.name = 'EMPLOYEES';
+binds.who = user;
+
+ctx.write('Using Binds:'+ binds + '\n');
+
+ctx.write(`
+  ***************************************************************
+  *****    SIMPLE LOOP OF LIST OF LIST                ***********
+  ***************************************************************\n`);
+
+var ret = util.executeReturnListofList(`select object_name,object_type
+                                          from  all_objects
+                                         where object_name = :name
+					                                 and owner = :who `,binds);
+
+for (var i = 0; i < ret.length; i++) {
+    ctx.write( ` ${ret[i][1]} \t  ${ret[i][0]} \n`);
+}
+
+ctx.write('\n\n');
+
+ctx.write(`
+  ***************************************************************
+  *****    SIMPLE LOOP OF LIST OF NAMES WITH BINDS     **********
+  ***************************************************************\n`);
+
+ret = util.executeReturnList(`select object_name,object_type
+                                from  all_objects
+			                         where object_name = :name
+			                           and owner = :who `,binds);
+
+for (i = 0; i < ret.length; i++) {
+    ctx.write( ` ${ret[i].OBJECT_TYPE} \t  ${ret[i].OBJECT_NAME}  \n`);
+}
