a61cfa5f4d9159d2326f5d25270c9b184346c8ab
Kris Rice <kris.rice@jokr.net>
2017-08-22 11:03:10 -0400

    Create color.sql

---
+++ sqlcl/examples/color.sql
@@ -0,0 +1,50 @@
+select '@|red,bold,underline This is red,bold,underline|@' "This is my Colorized column"  from dual
+union all
+select '@|NEGATIVE_ON  This is negative|@'   from dual
+union all
+select '@|INTENSITY_FAINT  This is faint|@'   from dual
+union all
+select '@|INTENSITY_BOLD  This is my bold|@'   from dual
+union all
+select '@|ITALIC  This is italic|@'   from dual
+union all
+select '@|UNDERLINE  This is underline|@'   from dual
+union all
+select '@|BLINK_SLOW  This is blink_slow|@'   from dual
+union all
+select '@|BLINK_FAST  This is blink_fast|@'   from dual
+union all
+select '@|CONCEAL_ON  This is conceal|@'   from dual
+union all
+select '@|black  This is black|@'   from dual
+union all
+select '@|green  This is green|@'   from dual
+union all
+select '@|yellow This is yellow|@'   from dual
+union all
+select '@|blue  This is blue|@'   from dual
+union all
+select '@|magenta  This is magenta|@'   from dual
+union all
+select '@|cyan   This is cyan|@'   from dual
+union all
+select '@|white  This is white|@'   from dual
+union all
+select '@|bg_red  This is bg_red|@'   from dual
+union all
+select '@|bg_black  This is bg_black|@'   from dual
+union all
+select '@|bg_green  This is bg_green|@'   from dual
+union all
+select '@|bg_yellow This is bg_yellow|@'   from dual
+union all
+select '@|bg_blue  This is bg_blue|@'   from dual
+union all
+select '@|bg_magenta  This is bg_magenta|@'   from dual
+union all
+select '@|bg_cyan   This is bg_cyan|@'   from dual
+union all
+select '@|bg_white  This is bg_white|@'   from dual
+union all
+select '@|bg_red  This is bg_red|@'   from dual
+/
