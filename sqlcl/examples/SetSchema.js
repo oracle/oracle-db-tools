0d7835304342af93c6f64eb0eca550b8a30fa9c2
Kris Rice <kris.rice@jokr.net>
2018-01-31 13:00:10 -0500

    Create SetSchema.js

---
+++ sqlcl/examples/SetSchema.js
@@ -0,0 +1,36 @@
+// SQLCL's Command Registry
+var CommandRegistry = Java.type("oracle.dbtools.raptor.newscriptrunner.CommandRegistry");
+
+// CommandListener for creating any new command
+var CommandListener =  Java.type("oracle.dbtools.raptor.newscriptrunner.CommandListener")
+
+
+// Broke the .js out from the Java.extend to be easier to read
+var cmd = {};
+
+// Called to attempt to handle any command
+cmd.handle = function (conn,ctx,cmd) {
+	var subCmd = cmd.getSql().trim().split(" ")
+	if ( subCmd[0].equalsIgnoreCase("s")){
+             sqlcl.setStmt('alter session set current_schema = ' + subCmd[1]);
+             sqlcl.run();
+
+	    return true;
+	}
+}
+// fired before ANY command
+cmd.begin = function (conn,ctx,cmd) {}
+
+// fired after ANY Command
+cmd.end = function (conn,ctx,cmd) {}
+
+// Actual Extend of the Java CommandListener
+
+var SetSchema = Java.extend(CommandListener, {
+        handleEvent: cmd.handle ,
+        beginEvent:  cmd.begin  ,
+        endEvent:    cmd.end
+});
+
+// Registering the new Command
+CommandRegistry.addForAllStmtsListener(SetSchema.class);
